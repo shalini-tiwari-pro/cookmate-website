7489e6040f638befb4cce770bb465c00380c3860
shalinitiwari2003 <shalinitiwariprayagraj@gmail.com>
2025-05-05 21:58:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".page");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    buttons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
